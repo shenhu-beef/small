3c7e12656713237fc7304b3f1d41e1ce666a66e3
chenlongtu <17538712780@163.com>
2021-04-27 16:46:22 +0800

    第二次初始化

---
+++ src/components/common/tabcontrol/TabControl.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="tab-control">
+        <div v-for="item in titles" class="tab-control-item">
+            <span>{{item}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TabControl',
+        props: {
+            titles: {
+                type: Array,
+                default () {
+                    return []
+                }
+            }
+        },
+        components: {
+
+        }
+    }
+</script>
+
+<style scoped>
+    .tab-control {
+        display: flex;
+        text-align: center;
+    }
+    
+    .tab-control-item {
+        flex: 1;
+        height: 40px;
+        line-height: 40px;
+        background-color: coral;
+    }
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -5,14 +5,22 @@
         </nav-bar>
         <home-swiper :banners="banners"></home-swiper>
         <recommend :recommends="recommends"></recommend>
+        <feature></feature>
+        <tab-control :titles="['流行', '新品', '精选']"></tab-control>
     </div>
 </template>
 
 <script>
-    import NavBar from 'components/common/navbar/NavBar'
+    //子组件
     import HomeSwiper from './childrencomps/HomeSwiper'
     import Recommend from './childrencomps/Recommend'
+    import Feature from './childrencomps/Feature'
 
+    //公共组件
+    import NavBar from 'components/common/navbar/NavBar'
+    import TabControl from 'components/common/tabcontrol/TabControl'
+
+    //其他组件
     import {
         getHomeMultidata
     } from 'network/home'
@@ -20,9 +28,11 @@
     export default {
         name: "Home",
         components: {
-            NavBar,
             HomeSwiper,
             Recommend,
+            Feature,
+            NavBar,
+            TabControl
         },
         data() {
             return {
@@ -41,7 +51,15 @@
 </script>
 
 <style scoped>
+    #home {
+        padding-top: 44px;
+    }
+    
     .home-nav {
+        position: fixed;
+        left: 0;
+        top: 0;
+        width: 100%;
         background-color: var(--color-tint);
         color: #fff
     }
